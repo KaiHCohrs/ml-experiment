abb2db4e13eb33e1610fb6f22c1a4d8d2f345746
Kai-Hendrik Cohrs <kai-hendrik.cohrs@hotmail.com>
2022-08-31 15:14:05 +0200

    Restructure folders

--- singularity_build.sh
+++
@@ -1,4 +0,0 @@
-export PATH=/usr/users/kcohrs/center-surround/docker/:$PATH
-source ~/.bashrc
-GPU=0 dockerrun --env-file env --jupyterport 9999 -name base_container eckerlabdocker/docker:cuda11.0-py3.8-torch1.7-tf2.4
-sudo singularity build base_container.sif docker-daemon://local/base_container
