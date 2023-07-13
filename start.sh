736e7d695e1be4b2eb08f4e9824d2b6e5c0fc58b
KaiHCohrs <88082519+KaiHCohrs@users.noreply.github.com>
2023-07-13 17:07:13 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,39 +1,26 @@
 #!/bin/bash
 
 # Ask for new repo name
-#read -p "Enter new repo name: " new_repo_name
+read -p "Enter new repo name: " new_repo_name
 
 # Rename directory
-#mv new_name/ "${new_repo_name}/"
+mv your_module_name/ "${new_repo_name}/"
 
 # Iterate over all Python files in the module and the script files and replace "your_module_name" with the new repo you have chosen
-#your_module_name
-#find "${new_repo_name}/" -type f -name "*.py" -print0 | xargs -0 sed -i "s/new_name/${new_repo_name}/g"
-#find "scripts/" -type f -name "*.py" -exec sed -i "s/new_name/${new_repo_name}/g" {} \;
+find "${new_repo_name}/" -type f -name "*.py" -print0 | xargs -0 sed -i "s/your_module_name/${new_repo_name}/g"
+find "scripts/" -type f -name "*.py" -exec sed -i "s/your_module_name/${new_repo_name}/g" {} \;
 
 # Substitute the name in setup.py file
-#sed -i "s/new_name/${new_repo_name}/g" "setup.py"
+sed -i "s/your_module_name/${new_repo_name}/g" "setup.py"
 
 # Build the conda environment from environment.yml
-#mamba env create -f environment.yml
+mamba env create -f environment.yml
 
 # Activate the conda environment
-#conda activate env-name
+conda activate ml-env
 
 # Install the module in editable mode
-#pip install -e .
-
-# Create and switch to development branch
-#git checkout -b dev
-
-# Add all changes to the staging area
-#git add -A
-
-# Create an initial commit with the specified message
-#git commit -m "-initial commit"
-
-# Push the changes to the remote repository
-#git push
+pip install -e .
 
 # Run hello world unit test
 python -m pytest tests/test_basic.py
@@ -42,4 +29,16 @@ python -m pytest tests/test_basic.py
 echo "You are all setup! Let's delete this file."
 
 # Delete this file
-#rm -- "$0"
+rm -- "$0"
+
+# Add all changes to the staging area
+git add -A
+
+# Create an initial commit with the specified message
+git commit -m "-initial commit"
+
+# Push the changes to the remote repository
+git push
+
+# Create and switch to development branch
+git checkout -b dev
